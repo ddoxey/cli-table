62fc92126daa8871f5fc433371f1797f925f758c
Dylan Doxey <dylan.doxey@gmail.com>
2020-12-23 02:38:03 +0000

    simplify main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -5,27 +5,23 @@
 namespace fs = boost::filesystem;
 
 
-void build_table(std::istream &data_ifs)
-{
-    Table data = Table(data_ifs);
-
-    std::cout << data;
-}
-
-
 int main(int ac, const char *av[])
 {
     fs::path filename(ac > 1 ? av[1] : "");
 
     if (filename == "")
     {
-        build_table(std::cin);
+        Table data = Table(std::cin);
+
+        std::cout << data;
     }
     else if (fs::is_regular_file(filename))
     {
         auto data_ifs = std::ifstream(filename.string(), std::ios::binary);
 
-        build_table(data_ifs);
+        Table data = Table(data_ifs);
+
+        std::cout << data;
     }
     else
     {
