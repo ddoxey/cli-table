90c8816f33ae9f3d08acff193c5e2003ea3cb4e3
Dylan Doxey <dylan.doxey@gmail.com>
2021-01-18 01:12:18 +0000

    add support for styling by JSON configuration

--- src/main.cpp
+++ src/main.cpp
@@ -1,24 +1,66 @@
 #include <fstream>
 #include "table.hpp"
+#include <nlohmann/json.hpp>
+
+using json = nlohmann::json;
 
 
 int main(int ac, const char *av[])
 {
-    if (ac < 2)
+    json style;
+    std::string filename;
+
+    for (int i = 1; i < ac; i++)
+    {
+        if (strcmp(av[i], "-s") == 0)
+        {
+            if (i + 1 == ac)
+                throw std::invalid_argument("missing style parameter: -s <json|filename>");
+
+            i++;
+            std::string line;
+            std::string style_text;
+            std::string style_source(av[i]);
+
+            auto style_ifs = std::ifstream(style_source, std::ios::binary);
+
+            if (style_ifs.good())
+            {
+                while (std::getline(style_ifs, line))
+                    style_text.append(line);
+            }
+            else
+            {
+                style_text = style_source;
+            }
+
+            style = json::parse(style_text);
+        }
+        else
+        {
+            filename = av[i];
+        }
+    }
+
+    if (filename.length() == 0)
     {
         Table data = Table(std::cin);
+
+        if (style > 0)
+            data.set_style(style);
 
         std::cout << data;
     }
     else
     {
-        std::string filename(av[1]);
-
         auto data_ifs = std::ifstream(filename, std::ios::binary);
 
         if (data_ifs.good())
         {
             Table data = Table(data_ifs);
+
+            if (style > 0)
+                data.set_style(style);
 
             std::cout << data;
         }
